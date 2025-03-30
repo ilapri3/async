d3d303dea7c8c9029f58d5cea532e4703d46e913
ilapri3 <ilyap20469@gmail.com>
2025-03-30 19:42:04 +0300

    task1

--- server.py
+++ server.py
@@ -1,31 +1,41 @@
 import asyncio
 
-HOST = 'localhost'
-PORT = 9095
+HOST = 'localhost'  # Хост для прослушивания
+PORT = 9095  # Порт для прослушивания
 
 
 async def handle_echo(reader, writer):
-    data = await reader.read(100)
-    message = data.decode()
+    """
+         Асинхронная функция для обработки подключений.
+    """
+    message = data.decode()  # Декодируем байты в строку
+    addr = writer.get_extra_info('peername')  # Получаем адрес клиента
+    print(f"Получено {message!r} от {addr}")
 
-    writer.write(data)
-    await writer.drain()
+    print(f"Отправка: {message!r}")
+    writer.write(data)  # Отправляем данные обратно клиенту (эхо)
+    await writer.drain()  # Ждем, пока данные будут отправлены
 
-    writer.close()
+    print("Закрытие соединения")
+    writer.close()  # Закрываем соединение
+    await writer.wait_closed()  # Ждем, пока соединение будет полностью закрыто
 
 
-loop = asyncio.get_event_loop()
-coro = asyncio.start_server(handle_echo, HOST, PORT, loop=loop)
-server = loop.run_until_complete(coro)
+async def main():
+    """
+    Основная асинхронная функция для настройки и запуска сервера.
+    """
+    # Запускаем сервер, который будет использовать функцию handle_echo для обработки подключений
+    server = await asyncio.start_server(handle_echo, HOST, PORT)
 
-# Serve requests until Ctrl+C is pressed
-print('Serving on {}'.format(server.sockets[0].getsockname()))
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+    # Получаем адреса, на которых сервер слушает
+    addrs = ', '.join(str(sock.getsockname()) for sock in server.sockets)
+    print(f'Сервер запущен на {addrs}')
 
-# Close the server
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+    async with server:
+        await server.serve_forever()  # Запускаем сервер и ждем подключений бесконечно
+
+
+if __name__ == '__main__':
+    # Запускаем основную функцию с помощью asyncio.run()
+    asyncio.run(main())
